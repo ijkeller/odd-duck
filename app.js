008862044fb1701921ba4a55b955210498910d39
Isaiah Keller <isaiah.keller@gmail.com>
2022-06-21 09:45:56 -0700

    started odd-duck project, added basic html, added imgArray and random function

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const imgArray = [bag.jpg, banana.jpg, bathroom.jpg, boots.jpg, breakfast.jpg, chair.jpg, cthulhu.jpg, dog-duck.jpg, dragon.jpg, pen.jpg, pet-sweep.jpg, scissors.jpg, shark.jpg, sweep.png, tauntaun.jpg, unicorn.jpg, water-can.jpg, wine-glass.jpg]
+const dir = './img/'
+
+let rounds = 25;
+
+const selectImage = function() {
+    Math.round(Math.random() * imgArray.length())
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
