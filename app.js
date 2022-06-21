202813cc82dcf1ef6b27032e3b13273392b43595
Isaiah Keller <isaiah.keller@gmail.com>
2022-06-21 10:18:00 -0700

    changed variable names

--- app.js
+++ app.js
@@ -3,12 +3,22 @@
 const imgArray = [bag.jpg, banana.jpg, bathroom.jpg, boots.jpg, breakfast.jpg, chair.jpg, cthulhu.jpg, dog-duck.jpg, dragon.jpg, pen.jpg, pet-sweep.jpg, scissors.jpg, shark.jpg, sweep.png, tauntaun.jpg, unicorn.jpg, water-can.jpg, wine-glass.jpg]
 const dir = './img/'
 
-let rounds = 25;
+let maxRounds = 25;
+let round = 0;
 
-const selectImage = function() {
+const randomIndex = function() {
     Math.round(Math.random() * imgArray.length())
 }
 
+const selectImage = `${dir}${imgArray[randomIndex()]}`
+
+const renderImage = function() {
+
+}
+
+while (round < maxRounds) {
+
+}
 
 
 
